754e532c85112570ba0e834d3702aba712fab6b8
Iago <iago.almeida@visie.com.br>
2025-02-18 12:27:08 -0300

    🩹 Tornando `SCRIPTS_TOKEN` ambientalizado

--- src/routers/scripts.py
+++ src/routers/scripts.py
@@ -1,3 +1,5 @@
+import os
+
 import fastapi
 
 from src import db
@@ -5,10 +7,12 @@ from src.scripts import seed
 
 router = fastapi.APIRouter(prefix='/scripts')
 
+SCRIPTS_TOKEN = os.getenv('SCRIPTS_TOKEN', 'batatafrita')
+
 
 @router.post('/seed', tags=['Scripts'])
 async def post_scripts_seed(token: str = fastapi.Header(None)):
-    if not token == 'batatafrita':
+    if not token == SCRIPTS_TOKEN:
         raise fastapi.HTTPException(401, 'Não autorizado')
     seed.main()
     return True
@@ -16,7 +20,7 @@ async def post_scripts_seed(token: str = fastapi.Header(None)):
 
 @router.post('/reset_db', tags=['Scripts'])
 async def post_scripts_reset_db(token: str = fastapi.Header(None)):
-    if not token == 'batatafrita':
+    if not token == SCRIPTS_TOKEN:
         raise fastapi.HTTPException(401, 'Não autorizado')
     db.reset()
     seed.main()
